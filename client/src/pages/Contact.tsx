bfceb4cea19a1e8ce1087ef345354e541a0ef2e8
Mekjunkong <89639158+Mekjunkong@users.noreply.github.com>
2026-04-03 23:23:30 +0700

    feat: update Google Maps embed to show WIRO 4x4 actual business location

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -457,7 +457,7 @@ export default function Contact() {
                       "WIRO 4x4 location - Chiang Mai, Thailand",
                       "מיקום WIRO 4x4 - צ'יאנג מאי, תאילנד"
                     )}
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241552.47849225957!2d98.8!3d18.79!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30da3a7e90bb6f5d%3A0x50d3c38c8e1e3b0!2sChiang%20Mai%2C%20Thailand!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3776.5!2d98.9!3d18.79!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x73908a4e39b362f3!2sWiro%204x4%20Indochina%20Adventure%20Tours!5e0!3m2!1sen!2sth!4v1700000000000!5m2!1sen!2sth"
                     width="100%"
                     height="100%"
                     style={{ border: 0, minHeight: "400px" }}
